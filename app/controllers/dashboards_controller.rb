399bc3baf81081c9d1d38a9140938f6608bd6d32
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-07-11 11:35:48 -0400

    Add styling to search results.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -3,5 +3,7 @@ class DashboardsController < ApplicationController
     @text_subject = TextSubject.new
     @image_subject = ImageSubject.new
     @timeline = current_user.timeline.page(params[:page]).per(5)
+    @all_shouts = Shout.all.order(created_at: :desc).limit(10)
+
   end
 end
